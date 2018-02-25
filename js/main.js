2b8190b68518a1039c5b8f95453e0111d184a98a
Stefan Bogdanović <bogdanovic.stefan@outlook.com>
2018-02-25 02:37:31 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,69 +1,39 @@
-// $(function () {
-//     $("nav").find("a[href*=#]:not([href=#])").click(function () {
-//         var offset = 0;
-//         var speed = 1000;
-//         var target = $(this.hash);
-//
-//         $("html,body").animate({
-//             scrollTop: target.offset().top - offset
-//         }, speed);
-//     });
-// });
-//var x = document.getElementById('close');
-//document.getElementById('close').addEventListener("click",close);
-//function close() {
-//  var paragraf = document.getElementById('para');
-//  paragraf.style.display='none';
-//};
-//console.log(x);
-var searchBox = document.querySelector('#searchBox');
-var searchBtn = document.querySelector('#searchBtn');
+let newsletter = document.querySelector('#newsletterInput')
+let dropdown = document.querySelector('#dropdown')
 
-searchBtn.addEventListener('click',function(e){
-  var searchText = document.querySelector('#searchBox').value;
-  //Dodati enter kad se stisne event code 13,razmotriti opciju sa blur kad je prazno i focus 
-    if(searchText ==""){
-      //alert("It mustn't be empty");
-      swal(
-        'It must not be empty.',
-        'Please fill in the search field.',
-        'error'
-      );
-      searchBox.style.borderColor = "#E74C3C"; //#1abc9c E74C3C
-      searchBox.classList.add("searchError"); // Dodati placeholder boju na crvenu?! 
-   }else{
-      alert(searchText); //.toUpperCase()
-      searchBox.style.borderColor = "white"; 
-    } 
-});
+document.body,addEventListener('load', () => {
+  newsletter.addEventListener('blur', newsletterValidatation)
+  dropdown.addEventListener('mouseenter',showDropdown)
+  
+})
 
-//Proveravanje newsletter-a
-var newsletterBtn = document.querySelector('#newsletterBtn');
-newsletterBtn.addEventListener('click',function () {
-  if(newsletterBox.value !=""){
-    swal('Thank you for your support.','You will be contacted soon.Check your '+ newsletterBox.value ,'success');
-    newsletterBox.value = " ";
-  }else{
-
+let newsletterValidatation = () => {
+  let inputVal = newsletter.value
+  let reEmail = /^$/
+  let span = document.querySelector('#res')
+  if (inputVal) {
+    span.classList.add('is-danger')
+    span.textContent = 'This email is invalid'
+  } else {
+    span.classList.add('is-success')
+    span.textContent = 'This email is valid'
   }
   
-});
-//Proveravanje e-mail-a iz newslettera
-//var reEmail ='/ /';
-//if(reEmail.test())
+}
+
+let showDropdown = () => {
+  dropdown.parentElement.classList.add('is-hoverable')
+}
 
 
-//Animirati searchBox
-//searchBtn.addEventListener('onmouseover',function(){
-//  searchBox.style.display="block";
-//})
 
-//Products i reviews odraditi kao traversy media https://www.youtube.com/watch?v=oRmQN244Ir0 POD OBAVEZNOOO!!!!
+/* VALIDATION */
+function validation() {
+   // let reName = / /
+   // let reLastName = / /
+   // let reName = / /
+   // let reName = / /
+   // let reName = / /
 
-//Image Slider
 
-var images=[];
-var counter =0;
-var max=images.length;
-
-console.log(max);
+}
