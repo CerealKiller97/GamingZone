edf2869d14fd582deaeccfb7d660a48f91d2f122
Stefan Bogdanović <bogdanovic.stefan@outlook.com>
2018-02-27 23:07:36 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -3,6 +3,8 @@ let progress = document.querySelector('#progress')
 let counter = 0
 let alertModal = document.querySelector('#alertModal')
 let registerBtn = document.querySelector('#registerBtn')
+let msgNotification = document.querySelector('#messageNotification')
+let notify = document.querySelector('#notify')
 
 document.addEventListener('DOMContentLoaded',() => {
   name.focus()
@@ -14,7 +16,26 @@ let registrationValidation = () => {
   let name = document.querySelector('#regName')
   let username = document.querySelector('#regUsername')
   let email = document.querySelector('#regEmail')
-  
+  /* Ispis teksta da li je dobro se registrovao ili ne */
+  /*IF => Ako je ok ona ovo */
+  if (!name.value) {
+    document.querySelector('#regName').classList.add('is-danger')
+    let messages = ['You have been successfully registered.', 'Please check your e-mail to confirm your identity.']
+    let lis = ``
+    for (let i in messages) {
+      lis += `<li> <strong>${messages[i]} </strong></li>`
+    }
+  notify.classList.add('is-link')
+  msgNotification.innerHTML = lis
+  } /* else {
+    let errors = ['You must.', 'aaa.']
+    let liErr = ``
+    for (let i in errors) {
+      liErr += `<li> <strong>${errors[i]} </strong></li>`
+    }
+    notify.classList.add('is-danger')
+    msgNotification.innerHTML = liErr
+  } */
 }
 
 let nameValid = () => {
@@ -26,6 +47,7 @@ let nameValid = () => {
     name.classList.add('is-success')
     progress.value = counter
     counter++
+    //animate za progress value
   }
   if (counter === 20) {
     progress.value = 20
@@ -43,14 +65,19 @@ let alert = (msg) => {
 
 $(document).ready(function () {
   // Register button
+  $('#notify').addClass('opacity')
 
   $('#registerBtn').click(function () {
-    $('#alert').slideToggle()
+    $('#notify').animate({
+      opacity: 1
+    })
 
   })
 
   $('#deleteBtn').click(function () {
-    $('#alert').fadeOut()
+    $('#notify').animate({
+      opacity: 0
+    })
   })
  
 
